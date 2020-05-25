c9d5512c0505c8e29c8860907cd0de9a3023570e
Valery Vargin <vdvugad@gmail.com>
2020-05-25 23:24:25 +0300

    Json auth provider

---
+++ api/tests/Controller/API/V0/AuthJsonControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller\API\V0;
+
+use App\DataFixtures\UserFixtures;
+use JsonException;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AuthJsonControllerTest extends WebTestCase
+{
+    /**
+     * @covers \App\Controller\API\V0\AuthJsonController::login
+     *
+     * @throws JsonException
+     */
+    public function testAuthJson(): void
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v0/auth/json',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+            ],
+            \json_encode(
+                [
+                    'email' => UserFixtures::EMAIL,
+                    'password' => UserFixtures::PASSWORD,
+                ],
+                JSON_THROW_ON_ERROR
+            )
+        );
+
+        static::assertResponseIsSuccessful();
+        static::assertResponseHeaderSame('content-type', 'application/json');
+
+        $response = $client->getResponse()->getContent();
+        $this->assertJson($response);
+
+        $data = \json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+
+        $this->assertIsArray($data);
+        $this->assertArrayHasKey('email', $data);
+        $this->assertEquals(UserFixtures::EMAIL, $data['email']);
+    }
+}
