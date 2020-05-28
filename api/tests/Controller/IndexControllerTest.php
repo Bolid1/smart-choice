fbaac4866657db4b5ece1ef27a71010b1a00b3c6
Valery <VDVUGaD@gmail.com>
2020-05-28 23:06:58 +0300

    Index page & layout (#4)
    To project was added index page and configured layout of all pages.

---
+++ api/tests/Controller/IndexControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    /**
+     * @covers \App\Controller\IndexController::index
+     */
+    public function testIndex(): void
+    {
+        static::createClient()->request('GET', '/register');
+
+        static::assertResponseStatusCodeSame(200);
+    }
+}
