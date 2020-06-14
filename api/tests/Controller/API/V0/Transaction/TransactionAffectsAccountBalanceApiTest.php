af254a1e5771a4b74e2091e91da2659103a243b3
Valery Vargin <vdvugad@gmail.com>
2020-06-14 19:45:42 +0300

    php doc fix

--- api/tests/Controller/API/V0/Transaction/TransactionAffectsAccountBalanceApiTest.php
+++ api/tests/Controller/API/V0/Transaction/TransactionAffectsAccountBalanceApiTest.php
@@ -51,10 +51,6 @@ class TransactionAffectsAccountBalanceApiTest extends ApiTestCase
      * @covers \App\DataPersister\TransactionDataPersister::supports()
      * @covers \App\DataPersister\TransactionDataPersister::persist()
      *
-     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
      * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
     public function testPatchTransaction(): void
