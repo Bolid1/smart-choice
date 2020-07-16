4c04952adb957e9754787baf4425aa87653ea715
Bolid1 <VDVUGaD@gmail.com>
2020-07-16 08:40:42 +0300

    Invalid test

--- api/tests/ImportPreparer/TransactionImportPreparerTest.php
+++ api/tests/ImportPreparer/TransactionImportPreparerTest.php
@@ -6,7 +6,9 @@ namespace App\Tests\ImportPreparer;
 
 use ApiPlatform\Core\Api\IriConverterInterface;
 use App\Converter\AccountConverter;
+use App\Converter\CategoryConverter;
 use App\Entity\Account;
+use App\Entity\Category;
 use App\ImportPreparer\TransactionImportPreparer;
 use PHPUnit\Framework\TestCase;
 
@@ -20,6 +22,7 @@ class TransactionImportPreparerTest extends TestCase
     {
         $preparer = new TransactionImportPreparer(
             $accountConverter = $this->createMock(AccountConverter::class),
+            $categoryConverter = $this->createMock(CategoryConverter::class),
             $converter = $this->createMock(IriConverterInterface::class),
         );
 
@@ -27,20 +30,28 @@ class TransactionImportPreparerTest extends TestCase
             'account' => 'Some account',
             'amount' => '123,65',
             'date' => '',
+            'categories' => 'Sport, Home',
         ];
         $context = [
             'foo' => 'bar',
         ];
 
         $accountConverter
-            ->expects($this->once())
+            ->expects(self::once())
             ->method('convert')
             ->with($data['account'], $context)
             ->willReturn($account = new Account())
         ;
 
+        $categoryConverter
+            ->expects(self::exactly(2))
+            ->method('convert')
+            ->withConsecutive(['Sport', $context], ['Home', $context])
+            ->willReturn($category1 = new Category(), $category2 = new Category())
+        ;
+
         $converter
-            ->expects($this->once())
+            ->expects(self::once())
             ->method('getIriFromItem')
             ->with($account)
             ->willReturn($accountIri = '/foo/bar/baz')
@@ -49,8 +60,9 @@ class TransactionImportPreparerTest extends TestCase
         $expected = [
             'account' => $accountIri,
             'amount' => 123.65,
+            'categories' => [$category1, $category2],
         ];
 
-        $this->assertEquals($expected, $preparer->prepare($data, $context));
+        self::assertEquals($expected, $preparer->prepare($data, $context));
     }
 }
